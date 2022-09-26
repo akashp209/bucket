5a67cfce98c8045845123c04ca6fd28ec6e5d8f9
Akash Patil <akashp209@gmail.com>
2022-09-26 10:40:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+// import S3FileUpload from 'react-s3';
+// import { uploadFile } from 'react-s3';
+// import keys from '../src/keys';
+import '../src/js/script'
+// import { upload } from '@testing-library/user-event/dist/upload';
+
+
+const config = {
+  bucketName: 'akreactbucket',
+  // dirName: 'photos', /* optional */
+  region: 'ap-south-1',
+  accessKeyId: 'AKIAXXLHD2SGSPEZS2XQ',
+  secretAccessKey: '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',
+};
+function upload(e) {
+  console.log(e.target);
+};
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Upload</h1>
+        <input type="file"  onChange={upload}/>
       </header>
     </div>
   );
---
+++ src/keys.js
@@ -0,0 +1,13 @@
+// // aws = {
+// //     key_id : AKIAXXLHD2SGSPEZS2XQ,
+// //     access_key : itDwTRC7rU2T8L/Zqgk85Vm/O6pXYo2XHVSWBNO6
+// // }
+
+// import React from 'react'
+
+// function keys() {
+//     var key_id = AKIAXXLHD2SGSPEZS2XQ;
+//     var access_key = itDwTRC7rU2T8L/Zqgk85Vm/O6pXYo2XHVSWBNO6;
+// }
+
+// export default keys( key_id, access_key)
